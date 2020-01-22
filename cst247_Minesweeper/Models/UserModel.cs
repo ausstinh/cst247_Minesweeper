fc2a6c499e5fd92b3a58c1431fe7699b1c468d8b
Austin Harvey <austin.harvey@deftflux.com>
2020-01-22 16:19:10 -0700

    test

--- cst247_Minesweeper/Models/UserModel.cs
+++ cst247_Minesweeper/Models/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,25 @@ namespace cst247_Minesweeper.Models
 {
     public class UserModel
     {
-        private string firstname {get; set;}
-        private string lastname { get; set; }
-        private string username { get; set; }
-        private string password { get; set; }
-        private string email { get; set; }
-        private string sex { get; set; }
-        private string age { get; set; }
-        private string state { get; set; }
+
+        [Required(ErrorMessage = "Firstname is required")]
+        [StringLength(15)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required")]
+        [StringLength(15)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(5)]
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Sex { get; set; }
+        public string Age { get; set; }
+        public string State { get; set; }
     }
 }
